27dd4a5922575a61c79e9297d3c511dc41ae6a9c
clee857 <chrysly0816@gmail.com>
2023-10-22 17:36:26 -0400

    Collider check

--- Assets/_Scripts/Actors/CassetteTape.cs
+++ Assets/_Scripts/Actors/CassetteTape.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,9 +8,11 @@ public class CassetteTape : MonoBehaviour {
     [SerializeField] private float bulletSpeed;
     [SerializeField] private float spread = 0.2f;
     private Rigidbody rb;
+    private bool collided = false;
     void Start() {
         rb = GetComponent<Rigidbody>();
         SetVelocity();
+        StartCoroutine(DestroyAction());
     }
 
     // Update is called once per frame
@@ -17,5 +20,22 @@ public class CassetteTape : MonoBehaviour {
         Random rand = new System.Random();
         if (rand.Next(0, 2) == 1) transform.right += new Vector3(0, (float) rand.NextDouble() * (spread + spread) - spread, (float) rand.NextDouble() * 1 - 0.5f);
         rb.velocity = transform.right * bulletSpeed;
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (collided && other.transform.GetComponent<Boss>() != null) {
+            collided = true;
+            other.transform.GetComponent<Boss>().HP -= 2;
+            
+            Destroy(transform.GetChild(1));
+            Destroy(transform.GetChild(2));
+            rb.velocity = Vector3.zero;
+        }
+    }
+
+    private IEnumerator DestroyAction() {
+        yield return new WaitForSeconds(5f);
+        Destroy(gameObject);
+        yield return null;
     }
 }
