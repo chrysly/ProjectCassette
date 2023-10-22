893d3d4e442d155738025647bd68e7f83dd0e262
clee857 <chrysly0816@gmail.com>
2023-10-22 14:58:18 -0400

    Main Menu UI

--- Assets/_Scripts/Actors/CassetteTape.cs
+++ Assets/_Scripts/Actors/CassetteTape.cs
@@ -5,6 +5,7 @@ using Random = System.Random;
 
 public class CassetteTape : MonoBehaviour {
     [SerializeField] private float bulletSpeed;
+    [SerializeField] private float spread = 0.2f;
     private Rigidbody rb;
     void Start() {
         rb = GetComponent<Rigidbody>();
@@ -14,7 +15,7 @@ public class CassetteTape : MonoBehaviour {
     // Update is called once per frame
     private void SetVelocity() {
         Random rand = new System.Random();
-        if (rand.Next(0, 2) == 1) transform.right += new Vector3(0, (float) rand.NextDouble() * 1 - 0.5f, (float) rand.NextDouble() * 1 - 0.5f);
+        if (rand.Next(0, 2) == 1) transform.right += new Vector3(0, (float) rand.NextDouble() * (spread + spread) - spread, (float) rand.NextDouble() * 1 - 0.5f);
         rb.velocity = transform.right * bulletSpeed;
     }
 }
---
+++ Assets/_Scripts/UI/MenuUI.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using TMPro;
+using UnityEngine;
+
+public class MenuUI : MonoBehaviour {
+    [SerializeField] private Transform spider;
+    private Vector3 spiderPos;
+    private Vector3 oldSpiderPos;
+
+    [SerializeField] private Transform mainText;
+    private Vector3 mainTextPos;
+    private Vector3 oldMainTextPos;
+
+    [SerializeField] private TMP_InputField textField;
+    private Vector3 textFieldPos;
+    private Vector3 oldTextFieldPos;
+    
+    [SerializeField] private Transform enterText;
+    private Vector3 enterTextPos;
+    private Vector3 oldEnterTextPos;
+
+    private bool inMainMenu = true;
+    
+    // Start is called before the first frame update
+    void Awake() {
+        spiderPos = spider.position;
+        mainTextPos = mainText.position;
+        textFieldPos = textField.transform.position;
+        enterTextPos = enterText.position;
+        Shift();
+    }
+
+    private void Shift() {
+        oldSpiderPos = new Vector3(spiderPos.x - 700, spiderPos.y, spiderPos.z);
+        oldMainTextPos = new Vector3(mainTextPos.x, mainTextPos.y + 300, mainTextPos.z);
+        oldTextFieldPos = new Vector3(textFieldPos.x, textFieldPos.y - 450, textFieldPos.z);
+        oldEnterTextPos = new Vector3(enterTextPos.x, enterTextPos.y - 400, enterTextPos.z);
+
+        spider.position = oldSpiderPos;
+        mainText.position = oldMainTextPos;
+        textField.transform.position = oldTextFieldPos;
+        enterText.position = oldEnterTextPos;
+    }
+
+    private void Start() {
+        StartCoroutine(StartAction());
+    }
+
+    private IEnumerator StartAction() {
+        spider.DOMove(spiderPos, 2f).SetEase(Ease.OutFlash);
+        yield return new WaitForSeconds(1.2f);
+        mainText.DOMove(mainTextPos, 1f).SetEase(Ease.OutBounce);
+        yield return new WaitForSeconds(0.5f);
+        textField.transform.DOMove(textFieldPos, 1f).SetEase(Ease.OutFlash);
+        yield return new WaitForSeconds(0.2f);
+        enterText.DOMove(enterTextPos, 1f).SetEase(Ease.OutFlash);
+        yield return null;
+    }
+
+    private IEnumerator HideAction() {
+        spider.DOMove(oldSpiderPos, 1f).SetEase(Ease.OutFlash);
+        mainText.DOMove(oldMainTextPos, 1f).SetEase(Ease.OutFlash);
+        textField.transform.DOMove(oldTextFieldPos, 1f).SetEase(Ease.OutFlash);
+        enterText.DOMove(oldEnterTextPos, 1f).SetEase(Ease.OutFlash);
+        yield return null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inMainMenu) {
+            if (Input.GetKeyDown(KeyCode.Return)) {
+                inMainMenu = false;
+                StartCoroutine(HideAction());
+            }
+        }
+    }
+}
