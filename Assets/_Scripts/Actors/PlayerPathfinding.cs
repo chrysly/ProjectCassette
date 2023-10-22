494af4ea797c0604d85aef813026ca3b55ca89c2
clee857 <chrysly0816@gmail.com>
2023-10-21 21:09:02 -0400

    Pixel Implementation sort of

---
+++ Assets/_Scripts/Actors/PlayerPathfinding.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPathfinding {
+    private List<Coil> _coils;
+    
+    public PlayerPathfinding(List<Coil> coils) {
+        _coils = coils;
+    }
+
+    public List<Coil> FindPath(Coil start) {
+        return null;
+    }
+}
